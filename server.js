f8fee6b2e71ce2158b19f05d4b0f65a5f633b817
CarlosAlv <carlitos290718@gmail.com>
2026-06-14 14:12:48 -0600

    Discord OAuth user fetch

--- server.js
+++ server.js
@@ -37,9 +37,55 @@ app.get("/auth/discord", (req, res) => {
 });
 app.get("/auth/discord/callback", async (req, res) => {
 
-  const code = req.query.code;
+  try {
 
-  res.send("Discord OAuth funcionando");
+    const code = req.query.code;
+
+    const tokenRes = await axios.post(
+      "https://discord.com/api/oauth2/token",
+      new URLSearchParams({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_CLIENT_SECRET,
+        grant_type: "authorization_code",
+        code,
+        redirect_uri: process.env.DISCORD_REDIRECT_URI
+      }),
+      {
+        headers: {
+          "Content-Type":
+            "application/x-www-form-urlencoded"
+        }
+      }
+    );
+
+    const accessToken =
+      tokenRes.data.access_token;
+
+    const userRes = await axios.get(
+      "https://discord.com/api/users/@me",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      }
+    );
+
+    const user = userRes.data;
+
+    res.json({
+      discordId: user.id,
+      username: user.username
+    });
+
+  } catch (err) {
+
+    console.error(err);
+
+    res.status(500).json({
+      error: "OAuth error"
+    });
+
+  }
 
 });
 client.once("ready", () => {
